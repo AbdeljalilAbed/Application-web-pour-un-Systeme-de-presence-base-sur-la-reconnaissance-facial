45cd3d47d7fe1ae91520157af0215bd1e0b5b298
dRAOUF1 <119814672+dRAOUF1@users.noreply.github.com>
2024-03-06 22:12:20 +0100

    fixed sah

--- Client2/src/components/Table.js
+++ Client2/src/components/Table.js
@@ -9,37 +9,43 @@ function Table() {
   useEffect(() => {
     axios
       .get("http://localhost:3001/getAggregatedData")
-      .then((res) => setEtds(res.data))
-      .catch((err) => console.log(err));
-      
-    axios
-      .get("http://localhost:3001/getGroupedDataForGroup2")
       .then((res) => {
-        setEtdsG2(res.data);
-        // Créer un objet avec les MatriculeEtd comme clés et false comme valeur par défaut
-        const defaultPresentStatus = res.data.reduce((acc, cur) => {
-          acc[cur.MatriculeEtd] = false;
-          return acc;
-        }, {});
-        setIsPresent(defaultPresentStatus);
+        setEtds(res.data);
+
+        // Obtenir les MatriculeEtd présents dans la réponse de getAggregatedData
+        const matriculesInAggregatedData = res.data.map(item => item.MatriculeEtd);
+
+        axios
+          .get("http://localhost:3001/getGroupedDataForGroup2")
+          .then((res) => {
+            setEtdsG2(res.data);
+            // Créer un objet avec les MatriculeEtd comme clés et initialiser à true
+            const defaultPresentStatus = res.data.reduce((acc, cur) => {
+              acc[cur.MatriculeEtd] = matriculesInAggregatedData.includes(cur.MatriculeEtd);
+              return acc;
+            }, {});
+            setIsPresent(defaultPresentStatus);
+          })
+          .catch((err) => console.log(err));
       })
       .catch((err) => console.log(err));
   }, []);
 
   const handleCheckboxChange = (MatriculeEtd, checked) => {
-    // Mettre à jour l'état local après la réussite de l'API
     if (checked) {
       axios
         .post("http://localhost:3001/postEtds", { matricule: MatriculeEtd })
         .then(() => {
-          setIsPresent((prev) => ({ ...prev, [MatriculeEtd]: true })); // Mettre à jour l'état isPresent
+          setIsPresent(prevState => ({ ...prevState, [MatriculeEtd]: true }));
+          console.log(`Added ${MatriculeEtd} to the collection presence`);
         })
         .catch((err) => console.log(err));
     } else {
       axios
         .delete(`http://localhost:3001/deleteEtd/${MatriculeEtd}`)
         .then(() => {
-          setIsPresent((prev) => ({ ...prev, [MatriculeEtd]: false })); // Mettre à jour l'état isPresent
+          setIsPresent(prevState => ({ ...prevState, [MatriculeEtd]: false }));
+          console.log(`Deleted ${MatriculeEtd} from the collection presence`);
         })
         .catch((err) => console.log(err));
     }
@@ -54,35 +60,29 @@ function Table() {
               <th>#</th>
               <th>Matricule</th>
               <th>Nom</th>
-              <th>Prenom</th>
-              <th>Presence</th>
+              <th>Prénom</th>
+              <th>Présence</th>
             </tr>
           </thead>
           <tbody>
-            {EtdsG2.map((Etd, index) => {
-              return (
-                <tr key={index}>
-                  <th scope="row">{index + 1}</th>
-                  <td>{Etd.MatriculeEtd}</td>
-                  <td>{Etd.nom}</td>
-                  <td>{Etd.prenom}</td>
-                  <td>
-                    <input
-                      key={index}
-                      type="checkbox"
-                      aria-label="Checkbox for following text input"
-                      checked={isPresent[Etd.MatriculeEtd]}
-                      onChange={(e) =>
-                        handleCheckboxChange(
-                          Etd.MatriculeEtd,
-                          e.target.checked
-                        )
-                      }
-                    />
-                  </td>
-                </tr>
-              );
-            })}
+            {EtdsG2.map((Etd, index) => (
+              <tr key={index}>
+                <th scope="row">{index + 1}</th>
+                <td>{Etd.MatriculeEtd}</td>
+                <td>{Etd.nom}</td>
+                <td>{Etd.prenom}</td>
+                <td>
+                  <input
+                    type="checkbox"
+                    aria-label="Checkbox for following text input"
+                    checked={isPresent[Etd.MatriculeEtd] || false}
+                    onChange={(e) =>
+                      handleCheckboxChange(Etd.MatriculeEtd, e.target.checked)
+                    }
+                  />
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
