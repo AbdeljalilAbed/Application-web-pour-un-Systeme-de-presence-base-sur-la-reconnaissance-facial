60b727886a14a177ec8750821f86978254581057
dRAOUF1 <119814672+dRAOUF1@users.noreply.github.com>
2024-03-16 18:47:22 +0100

    added getIdCreneau to presence

--- Server/models/presence.js
+++ Server/models/presence.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
+const getIdCreneau = require("../utils");
 
 const presenceSchema = new mongoose.Schema({
   matricule: String,
   date: { type: Date, default: Date.now },
-  creneau: {type:Number, default: 1},
+  creneau: {type:Number, default: getIdCreneau()},
 });
 
 const PModel = mongoose.model("presence", presenceSchema);
